92566e2baed2caada2b8302770f003f9862c0f8e
AnnaMelekh <annasmelekhina@gmail.com>
2022-01-17 00:33:13 +0300

    added private funds

--- SomeJson/InfoModel.swift
+++ SomeJson/InfoModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Info {
+struct TextBlock: Decodable {
     let userID: Int
     let title: String
     let body: String
--- SomeJson/MainViewController.swift
+++ SomeJson/MainViewController.swift
@@ -18,13 +18,41 @@ class MainViewController: UIViewController {
 //    }
 //
     @IBAction func pushButtonTwo() {
+        buttonTwoPressed()
     }
     
-    @IBAction func pushButtonThree(_ sender: UIButton) {
+    @IBAction func pushButtonThree() {
+    }
+
+// MARK: - Private Methods
+private func successAlert() {
+    DispatchQueue.main.async {
+        let alert = UIAlertController(
+            title: "Success",
+            message: "You can see the results in the Debug area",
+            preferredStyle: .alert
+        )
+        
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        self.present(alert, animated: true)
     }
 }
 
-
+private func failedAlert() {
+    DispatchQueue.main.async {
+        let alert = UIAlertController(
+            title: "Failed",
+            message: "You can see error in the Debug area",
+            preferredStyle: .alert
+        )
+        
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        self.present(alert, animated: true)
+    }
+}
+}
 
 //MARK: Networking
 
@@ -37,6 +65,15 @@ extension MainViewController {
                 print(error?.localizedDescription ?? "No error description")
                 return 
             }
+        
+            do {
+                let text = try JSONDecoder().decode(TextBlock.self, from: data)
+                self.successAlert()
+                print(text)
+            } catch {
+                self.failedAlert()
+                print(error.localizedDescription)
+            }
             
         }.resume()
 }
@@ -45,7 +82,7 @@ extension MainViewController {
         guard let url = URL(string: Link.thirdButton.rawValue) else { return }
         
         URLSession.shared.dataTask(with: url) { data, _, error in
-            guard let data = data else {
+            guard let _ = data else {
                 print(error?.localizedDescription ?? "No error description")
                 return
             }
