f903ffb4b4809941923a372150c5177fc1b53f46
AnnaMelekh <annasmelekhina@gmail.com>
2022-01-16 23:48:37 +0300

    added picture created model for Json

---
+++ SomeJson/DataManager.swift
@@ -0,0 +1,13 @@
+//
+//  DataManager.swift
+//  SomeJson
+//
+//  Created by Anna Melekhina on 16.01.2022.
+//
+
+import Foundation
+
+enum Link: String {
+    case imageURL = "https://www.meme-arsenal.com/memes/0f3e4d28fc2de15c91a6f83e19f7aa68.jpg"
+    case secondButton = ""
+}
---
+++ SomeJson/InfoModel.swift
@@ -0,0 +1,15 @@
+//
+//  InfoModel.swift
+//  SomeJson
+//
+//  Created by Anna Melekhina on 16.01.2022.
+//
+
+import Foundation
+
+struct Info {
+    let userID: Int
+    let title: String
+    let body: String
+ }
+
